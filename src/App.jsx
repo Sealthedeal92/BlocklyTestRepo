5e98b59c29295b5c4047cbb2112ac6e6152c82c7
Sealthedeal92 <141970691+Sealthedeal92@users.noreply.github.com>
2025-05-26 05:12:03 -0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import BlocklyEditor from './BlocklyEditor';
+
+const App = () => (
+  <div>
+    <h1>EZPawn - SourceMod Visual Editor</h1>
+    <BlocklyEditor />
+  </div>
+);
+
+export default App;
